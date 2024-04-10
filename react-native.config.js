a3ccf406552520be08edfe09d6e05c5b76d3fdd3
Saad Najmi <sanajmi@microsoft.com>
2024-04-10 05:35:09 -0500

    Refactor test app

---
+++ react-native.config.js
@@ -0,0 +1,49 @@
+const project = (() => {
+  const fs = require('fs');
+  const path = require('path');
+  try {
+    const { configureProjects } = require('react-native-test-app');
+
+    return configureProjects({
+      android: {
+        sourceDir: path.join('example', 'android'),
+        manifestPath: path.join(__dirname, 'example', 'android'),
+      },
+      ios: {
+        sourceDir: 'example/ios',
+      },
+      windows: fs.existsSync('example/windows/date-time-picker-example.sln') && {
+        sourceDir: path.join('example', 'windows'),
+        solutionFile: path.join('example', 'windows', 'date-time-picker-example.sln'),
+        project: path.join(__dirname, 'example', 'windows'),
+      },
+    });
+  } catch (e) {
+    return undefined;
+  }
+})();
+
+module.exports = {
+  dependencies: {
+    // Help rn-cli find and autolink this library
+    '@react-native-community/datetimepicker': {
+      root: __dirname,
+    },
+  },
+  dependency: {
+    platforms: {
+      windows: {
+        sourceDir: 'windows',
+        solutionFile: 'DateTimePickerWindows.sln',
+        projects: [
+          {
+            projectFile: 'DateTimePickerWindows/DateTimePickerWindows.vcxproj',
+            directDependency: true,
+          },
+        ],
+      },
+    },
+  },
+  ...(project ? { project } : undefined),
+};
+
